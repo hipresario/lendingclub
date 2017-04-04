6c42fdb3450bac28aa6fff8dff6117775801007f
hipresario <y070049@gmail.com>
2017-04-04 15:24:11 +0800

    update data

--- lc.py
+++ lc.py
@@ -4,10 +4,16 @@ from sklearn.model_selection import StratifiedShuffleSplit
 import math
 
 def cleanData(src):
+    """
+    remove loans with current loan status
+    :param src:
+    :return:
+    """
     rawdata = pd.read_csv(src, encoding='latin-1')
     cleandata = rawdata[rawdata['loan_status'] != 'Current']
     cleandata.to_csv('loan_clean.csv', index=False, encoding="latin-1" )
 
+#selected attributes for experiment
 fields = ['loan_amnt','int_rate','installment','sub_grade',
           'emp_length','home_ownership','annual_inc',
           'verification_status','loan_status',
@@ -15,8 +21,8 @@ fields = ['loan_amnt','int_rate','installment','sub_grade',
           'earliest_cr_line','inq_last_6mths','open_acc',
           'revol_bal','revol_util','initial_list_status']
 
-HOME = ['MORTGAGE', 'RENT',  'OWN']
-HOME_VALUE = [1, 2, 3]
+# HOME = ['MORTGAGE', 'RENT',  'OWN']
+# HOME_VALUE = [1, 2, 3]
 
 # PURPOSE = ['car', 'credit_card', 'debt_consolidation', 'home_improvement', 'house',
 #             'major_purchase', 'medical', 'moving', 'renewable_energy', 'small_business',
@@ -24,6 +30,7 @@ HOME_VALUE = [1, 2, 3]
 #             ]
 # PURPOSE_VALUE = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
+#convert grades to numericals
 SUB_GRADE = ['A1', 'A2', 'A3', 'A4', 'A5',
              'B1', 'B2', 'B3', 'B4', 'B5',
              'C1', 'C2', 'C3', 'C4', 'C5',
@@ -41,47 +48,92 @@ SUB_GRADE_VALUE = [1, 2, 3, 4, 5,
                    31, 32, 33, 34, 35
                    ]
 
+#convert years to numericals
 YEARS = [' ', 'n/a', '< 1 year', '1 year', '2 years', '3 years', '4 years', '5 years', '6 years', '7 years',
          '8 years', '9 years', '10 years', '10+ years']
 YEARS_VALUE = [0, 0, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10]
 
+#prediction class conversion
 LOAN_STATUS = ['Default', 'Charged Off', 'In Grace Period', 'Late (16-30 days)', 'Late (31-120 days)', 'Fully Paid']
 LOAN_STATUS_VALUE = ['BAD', 'BAD', 'BAD', 'BAD', 'BAD', 'GOOD']
 
+def logValue(row, label):
+    return math.log10(row[label])
+
 def fixYear(x):
+    """
+    fix employment length
+    :param x:
+    :return:
+    """
     if not x or x == '':
         return 'n/a'
-    return x;
+    return x
+
+def calculateCreditYear(row):
+    """
+    convert earliest credit years
+    :param row:
+    :return:
+    """
+    year = str(row['earliest_cr_line'])
+    if not year or year.strip() == '':
+        return 'n/a'
+    years = year.split('-')
+    num = float(years[1][0])
+    if num > 2:
+        years = '19' + years[1]
+    else:
+        years = '20' + years[1]
+    dif = 2012 - float(years)
+
+    return dif
 
 def p2f(x):
+    """
+    percentage strings to numbers in float
+    :param x:
+    :return:
+    """
     if not x or x == 'n/a' or x == '':
         return 0
     x = x.strip('%')
     return float(x)/100
 
 def selectData(src):
+    """
+    filter selected data only
+    :param src:
+    :return:
+    """
     df = pd.read_csv(src, encoding='latin-1',  skipinitialspace=True, usecols=fields,
                      na_values = {'n/a','na', ''},
                     converters={'int_rate':p2f,
                                 'revol_util': p2f,
-                                'emp_length': fixYear,
+                                'emp_length': fixYear
+
                                 })
+
+    df['credit_years'] = df.apply(calculateCreditYear, axis=1)
 
     df['initial_list_status'].replace(
         to_replace=['w', 'f'],
         value=[1, 0],
         inplace=True
     )
+
     df['emp_length'].replace(
         to_replace=YEARS,
         value=YEARS_VALUE,
         inplace=True
     )
+
     df['verification_status'].replace(
         to_replace=['Source Verified', 'Verified', 'Not Verified'],
         value=[0, 0, 1],
         inplace=True
     )
+
     df['loan_status'].replace(
         to_replace=LOAN_STATUS,
         value=LOAN_STATUS_VALUE,
@@ -96,8 +148,11 @@ def selectData(src):
 
     df.to_csv('loan_2010_12_clean.csv', index=False)
 
-
 def createDummyVar():
+    """
+    create dummmy variables
+    :return:
+    """
     df = pd.read_csv('loan_2010_12_clean.csv', encoding='latin-1')
     df['installment_to_income'] = (12 * df['installment'])/ df['annual_inc']
     df['revol_to_income'] = (12 * df['revol_bal']) / df['annual_inc']
@@ -108,8 +163,10 @@ def createDummyVar():
     df.to_csv('loan_2010_12_with_dummy.csv', index=False)
 
 def splitData():
-    """Training, Validation, Test 6:2:2"""
-    all = pd.read_csv('2016_LC.csv', encoding='latin-1')
+    """
+    Training, Validation, Test 60%:20%:20%
+    """
+    all = pd.read_csv('loan_2010_12_with_dummy.csv', encoding='latin-1')
     X = (all.drop(['loan_status'], axis=1))
     y = all['loan_status']
 
@@ -133,7 +190,7 @@ def main():
     #src = 'loan.csv'
     #cleanData(src)
     src = 'loan_clean.csv'
-    #selectData(src)
+    selectData(src)
     createDummyVar()
     # #statistics()
     #combineData()
--- modelling.py
+++ modelling.py
@@ -2,6 +2,7 @@
 # @date April 4, 2017
 # @y070049@gmail.com
 import pandas as pd
+import math
 #from sklearn.decomposition import PCA
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
@@ -13,6 +14,8 @@ from sklearn.model_selection import cross_val_score
 #from sklearn.random_projection import sparse_random_matrix
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import StratifiedKFold
 
 def output():
     test = pd.read_csv('test_final.csv')
@@ -39,33 +42,54 @@ def main():
     X = (all.drop(dropFields, axis=1))
     y = all['loan_status']
 
+    # split training and test 80% : 20%
+    # training using 4-fold cross validation i.e. validation 20% of the total dataset
     sss = StratifiedShuffleSplit(test_size=0.2)
 
     for train_index, test_index in sss.split(X, y):
         X_train, X_test = X.iloc[train_index], X.iloc[test_index]
         y_train, y_test = y[train_index], y[test_index]
 
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+    # Now apply the transformations to the data:
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
+
     #logistic regression
-    rf = LogisticRegression( class_weight='balanced', random_state = 10,
+    lr = LogisticRegression( class_weight='balanced', random_state = 10, C=0.5,
+                             penalty='l2',dual = False,
                              solver = 'liblinear',n_jobs = -1
                              )
-
-    rf.fit(X_train, y_train)
-    print(rf.score(X_train, y_train))
-    test_pred = rf.predict(X_test)
+    #
+    lr.fit(X_train, y_train)
+    scores = cross_val_score(lr, X_train, y_train, cv=4)
+    #print(lr.score(X_train, y_train))
+    print(lr.coef_)
+    print(scores)
+    test_pred = lr.predict(X_test)
     score = accuracy_score(y_test, test_pred)
     print(score)
     c1 = confusion_matrix(y_test, test_pred, labels = ['GOOD', 'BAD'])
     print(c1)
+    return
     # X_test['expected'] = y_test
     # X_test['predict'] = test_pred
     # X_test.to_csv('result.csv', index=False)
     #
 
     #random forest
-    
-
-
+    rf = RandomForestClassifier(n_estimators=60, oob_score=True, criterion='gini', max_features=4,
+                                max_depth = 8,
+                                n_jobs=-1, random_state=10, min_samples_leaf=1,
+                                class_weight='balanced', warm_start=False
+                                )
+    rf.fit(X_train,y_train)
+    print(rf.score(X_train, y_train))
+    preds = rf.predict(X_test)
+    score = accuracy_score(y_test, preds)
+    c2 =confusion_matrix(y_test, preds, labels=['GOOD', 'BAD'])
+    print(c2)
 
 if __name__ == '__main__':
     main()
